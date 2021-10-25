fea2d4d797f8a822a1365bc8916cd9fc57a9c230
Gokulapriya-Arunachalam <91065590+Gokulapriya-Arunachalam@users.noreply.github.com>
2021-10-25 17:56:32 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -3,20 +3,31 @@
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     s.average = 0;
+    s.min = 0;
+    s.max = 0;
     int i =0;
+    if((setlength == 0) || (numberset == 0) || (numberset == NULL))
+    {
+        s.average = NAN;
+        s.min = NAN;
+        s.max = NAN;
+    }
+    else
+    {
     s.min = numberset [i];
     s.max = numberset [i];
-    for(i=0; i < setlength; i++)
-    {
-        s.average = s.average + numberset [i];
-        if(numberset [i]< s.min)
+        for(i=0; i < setlength; i++)
         {
-            s.min =numberset [i];
+            s.average = s.average + numberset [i];
+            if(numberset [i]< s.min)
+            {
+                s.min =numberset [i];
+            }
+            if(numberset [i]> s.max)
+            {
+                s.max =numberset [i];
+            }            
         }
-        if(numberset [i]> s.max)
-        {
-            s.max =numberset [i];
-        }            
     }
         s.average = s.average / setlength;
     return s;
