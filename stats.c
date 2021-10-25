bfbb5d5ff1e2985dcb2c41da54a1a1febbb8f86e
Gokulapriya-Arunachalam <91065590+Gokulapriya-Arunachalam@users.noreply.github.com>
2021-10-25 17:33:00 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -5,6 +5,34 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    int i =0;
+    
+    if((numberset == 0) || (setlength == 0) || (numberset == NULL))
+    {
+        s.average = NAN;
+        s.min = NAN;
+        s.max = NAN;
+    }
+    else
+    {
+        s.average = 0.0;
+        s.min = numberset [i];
+        s.max = numberset [i];
+        for(i=0; i < setlength; i++)
+        {
+            s.average = s.average + numberset [i];
+            
+            if(numberset [i]< s.min)
+            {
+                s.min =numberset [i];
+            }
+            if(numberset [i]> s.max)
+            {
+                s.max =numberset [i];
+            }            
+        }
+        s.average = s.average / setlength;
+    }
 }
 
 int emailAlertCallCount = 0;
